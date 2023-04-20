eee6b093ba460ad8d3dac85ab92f2ecb5609f04b
Ghost Ops <72981180+GhostOps77@users.noreply.github.com>
2023-04-20 22:24:33 +0530

    modified:   tests/test_command_collection.py 	modified:   tests/test_completion/test_common_tests/test_hidden_cmd_and_args.py

--- tests/test_command_collection.py
+++ tests/test_command_collection.py
@@ -50,3 +50,38 @@ c2 = ClickCompleter(cmd, click.Context(cmd))
 def test_subcommand_invocation_from_group(test_input, expected):
     completions = c2.get_completions(Document(test_input))
     assert {x.text for x in completions} == expected
+
+
+@click.group()
+def root_group():
+    pass
+
+
+@root_group.group()
+def first_level_command():
+    pass
+
+
+@first_level_command.command()
+def second_level_command_one():
+    pass
+
+
+@first_level_command.command()
+def second_level_command_two():
+    pass
+
+
+c3 = ClickCompleter(root_group, click.Context(root_group))
+
+
+@pytest.mark.parametrize("test_input, expected", [
+    (" ", {"first-level-command"}),
+    ("first-level-command ", {
+        "second-level-command-one",
+        "second-level-command-two",
+    })
+])
+def test_completion_multilevel_command(test_input, expected):
+    completions = c3.get_completions(Document(test_input))
+    assert set(x.text for x in completions) == expected
--- tests/test_completion/test_common_tests/test_hidden_cmd_and_args.py
+++ tests/test_completion/test_common_tests/test_hidden_cmd_and_args.py
@@ -46,38 +46,3 @@ def args_choices_hidden_cmd(handler):
 def test_args_of_hidden_command(test_input):
     completions = c.get_completions(Document(test_input))
     assert {x.text for x in completions} == set()
-
-
-@click.group()
-def root_group():
-    pass
-
-
-@root_group.group()
-def first_level_command():
-    pass
-
-
-@first_level_command.command()
-def second_level_command_one():
-    pass
-
-
-@first_level_command.command()
-def second_level_command_two():
-    pass
-
-
-c2 = ClickCompleter(root_group, click.Context(root_group))
-
-
-@pytest.mark.parametrize("test_input, expected", [
-    (" ", {"first-level-command"}),
-    ("first-level-command ", {
-        "second-level-command-one",
-        "second-level-command-two",
-    })
-])
-def test_completion_multilevel_command(test_input, expected):
-    completions = c2.get_completions(Document(test_input))
-    assert set(x.text for x in completions) == expected
